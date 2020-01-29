f9a2ae943e178ecbdaa283bc1ce2941546983996
Josiah  Partridge <jpartridj94@gmail.com>
2020-01-29 03:18:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,3 +15,11 @@ def position_taken?(board, index)
     true
   end
 end
+
+def display_board(board_array)
+  puts " #{board_array[0]} | #{board_array[1]} | #{board_array[2]} "
+  puts "-----------"
+  puts " #{board_array[3]} | #{board_array[4]} | #{board_array[5]} "
+  puts "-----------"
+  puts " #{board_array[6]} | #{board_array[7]} | #{board_array[8]} "
+end
