0cc80c212ae07fffb6855086cd82c69950d31ef8
Josiah  Partridge <jpartridj94@gmail.com>
2020-01-29 03:30:26 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.chomp
   clean_input = input_to_index(input)
-  if valid_move(board, clean_input) == true
+  if valid_move?(board, clean_input) == true
     move(board, clean_input)
   else
     puts "That input is not valid. Pleae input a number between 1 and 9"
